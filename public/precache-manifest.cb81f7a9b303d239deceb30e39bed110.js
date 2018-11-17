b6c412cf63d92155004f9345abb1935d344b659b
borgatajunky <mad1083@gmail.com>
2018-11-17 01:19:39 -0500

    pushing to heroku

---
+++ public/precache-manifest.cb81f7a9b303d239deceb30e39bed110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/spacexlogo.b15ef947.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b3eb163c219041e7aabc",
+    "url": "/static/js/main.b3eb163c.chunk.js"
+  },
+  {
+    "revision": "4a167f135087355447ae",
+    "url": "/static/js/1.4a167f13.chunk.js"
+  },
+  {
+    "revision": "b3eb163c219041e7aabc",
+    "url": "/static/css/main.ff2fb641.chunk.css"
+  },
+  {
+    "revision": "44d0188cabef1db124d4509d83a757eb",
+    "url": "/index.html"
+  }
+];
